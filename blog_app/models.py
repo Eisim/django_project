6756082ac8e5f344992137505dca3be26a143d19
VKN <koctik.w@mail.ru>
2026-05-30 00:19:19 +0300

    code state after lesson-2

---
+++ blog_app/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    views_count = models.IntegerField(default=0)
