eda2146fb23cd035d2f744c0b99251046bc78575
VKN <koctik.w@mail.ru>
2026-06-06 00:50:56 +0300

    state:1,2 tasks done

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -7,5 +7,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts/', views.posts_list, name='posts_list'),
-    path('posts/<int:post_id>/', views.post_detail, name='post_detail')
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('categories/', views.categories_list, name='categories_list'),
+    path('categories/<int:category_id>/', views.category_detail, name='category_detail'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,21 +1,27 @@
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Category
 from django.shortcuts import get_object_or_404
 
 def index(request):
     return HttpResponse("<h1>Hello blog!</h1>")
 
 
+def __http_post_response(post:Post) -> str:
+    return f'<li>{post.created_at}) <a href=\"/posts/{post.id}/\">{post.title}</a> | {post.author}</li>'
+
+
+def __http_category_response(category: Category) -> str:
+    return f'<li><a href="/categories/{category.id}">{category.title}</a></li>'
+
 def posts_list(request):
-    def http_post_response(post:Post) -> str:
-        return f'<li>{post.created_at}) <a href=\"/posts/{post.id}/\">{post.title}</a> | {post.author}</li>'
 
     posts = Post.objects.filter(published=True)
     response_header = "<h1>Published posts</h1>"
-    posts_list = f"<ul>{''.join([http_post_response(post) for post in posts])}</ul>"
-    http_response = f"{response_header}\n{posts_list}"
+    post_list = f"<ul>{''.join([__http_post_response(post) for post in posts])}</ul>"
+    http_response = f"{response_header}\n{post_list}"
 
     return HttpResponse(http_response)
+
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
@@ -23,4 +29,20 @@ def post_detail(request, post_id):
     post_author = f"by: {post.author}"
     post_content = f"{post.content}"
     http_response = f"{response_header}\n{post_content}<br>{post_author}"
+    return HttpResponse(http_response)
+def categories_list(request):
+    categories = Category.objects.all()
+    response_header = "<h1>Categories</h1>"
+    category_list = f"<ul>{''.join([__http_category_response(category) for category in categories])}</ul>"
+    http_response = f"{response_header}\n{category_list}"
+    return HttpResponse(http_response)
+
+def category_detail(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    response_header = f"<h1>{category.title}</h1>"
+    posts = Post.objects.filter(category=category, published=True)
+
+    post_list = f"<ul>{''.join([__http_post_response(post) for post in posts])}</ul>"
+
+    http_response = f"{response_header}\n{post_list}"
     return HttpResponse(http_response)
