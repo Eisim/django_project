497b1b13805e651e598053f2880791856afa4e41
VKN <koctik.w@mail.ru>
2026-06-24 03:58:44 +0300

    state: change view to CBV

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -3,14 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='index'),
-    path('posts/', views.posts_list, name='posts_list'),
+    path('posts/', views.PostListPageView.as_view(), name='posts_list'),
     path('posts/create/', views.PostCreateView.as_view(), name='post_create'),
     path('posts/<slug:post_slug>/edit', views.PostUpdateView.as_view(), name='post_edit'),
     path('posts/<slug:post_slug>/delete', views.PostDeleteView.as_view(), name='post_delete'),
 
     path('posts/<slug:post_slug>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('categories/', views.categories_list, name='categories_list'),
-    path('categories/create/', views.category_create, name='category_create'),
-    path('categories/<int:category_id>/', views.category_detail, name='category_detail'),
+    path('categories/', views.CategoryListView.as_view(), name='categories_list'),
+    path('categories/create/', views.CategoryCreateView.as_view(), name='category_create'),
+    path('categories/<int:category_id>/', views.CategoryDetailView.as_view(), name='category_detail'),
 ]
 app_name = 'blog'
