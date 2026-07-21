aac13c62b4b44b67325962993b69414203985ba7
VKN <koctik.w@mail.ru>
2026-07-21 23:07:41 +0300

    state after homework: add category serializer + APIView; add custom field posts_count to CategorySerializer class

--- drf_app/serializers.py
+++ drf_app/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
-from blog_app.models import Post
+from blog_app.models import Post, Category
+
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id', 'title','slug', 'content', 'category', 'author', 'published', 'created_at']
+        fields = ['id', 'title', 'slug', 'content', 'category', 'author', 'published', 'created_at']
         read_only_fields = ['created_at']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    posts_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Category
+        fields = ['title', 'slug', 'id', 'posts_count']
+
+    def get_posts_count(self, obj) -> int:
+        post_count = Post.objects.filter(category=obj.id).count()
+        return post_count
--- drf_app/urls.py
+++ drf_app/urls.py
@@ -8,4 +8,7 @@ app_name='drf_app'
 urlpatterns = [
     path('posts/', api.PostListCreateAPIView.as_view(),name='post-list-create'),
     path('posts/<int:pk>', api.PostRetrieveUpdateDestroyAPIView.as_view(),name='post-detail'),
+
+    path('categories/', api.CategoryListCreateAPIView.as_view(),name='category-list-create'),
+    path('categories/<int:pk>', api.CategoryRetrieveUpdateDestroyAPIView.as_view(),name='category-detail'),
 ]
