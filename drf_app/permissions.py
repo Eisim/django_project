14718f791f12a65ba9d0f2439b0dbf28b9fd0978
VKN <koctik.w@mail.ru>
2026-07-22 02:29:58 +0300

    remove trash branches

--- drf_app/permissions.py
+++ drf_app/permissions.py
@@ -5,4 +5,4 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         is_safe = request.method in permissions.SAFE_METHODS
         is_author = obj.author == request.user
-        return (is_author) or is_safe
+        return is_author or is_safe
