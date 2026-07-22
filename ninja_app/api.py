9fd03c677f0141cbaf8f874d0901bd7e5e1549a3
VKN <koctik.w@mail.ru>
2026-07-22 16:59:22 +0300

    state after homework: add category schema + CRUD routes

--- ninja_app/api.py
+++ ninja_app/api.py
@@ -2,8 +2,8 @@ from ninja import Router
 from ninja.errors import HttpError
 from slugify import slugify
 
-from blog_app.models import Post
-from ninja_app.schemas import PostOutSchema, PostInSchema
+from blog_app.models import Post, Category
+from ninja_app.schemas import PostOutSchema, PostInSchema, CategoryOutSchema, CategoryInSchema
 
 from django.contrib.auth.models import User
 
@@ -19,7 +19,6 @@ async def ping(request):
 
 @router.get(path="/posts", response=list[PostOutSchema])
 async def posts(request, search: str | None = None, category_id: int | None = None):
-
     query_set = Post.objects.filter(published=True)
 
     if search:
@@ -54,3 +53,52 @@ async def create_post(request, payload: PostInSchema):
 
     new_post = await Post.objects.acreate(**post_data)
     return 201, new_post
+
+
+@router.get('/categories', response=list[CategoryOutSchema])
+async def categories(request, search_title: str | None = None):
+    query_set = Category.objects.all()
+    if search_title:
+        query_set = query_set.filter(title__icontains=search_title)
+
+    categories = [category async for category in query_set]
+    return categories
+
+
+@router.get('/categories/{category_id}', response=CategoryOutSchema)
+async def get_category(request, category_id: int):
+    try:
+        category = await Category.objects.aget(id=category_id)
+    except Category.DoesNotExist:
+        raise HttpError(404, "Категория не найдена")
+    return category
+
+
+@router.post('/categories', response={201: CategoryOutSchema})
+async def create_category(request, payload: CategoryInSchema):
+    category_data = payload.dict()
+    new_category, created = await Category.objects.aget_or_create(**category_data)
+
+    if not created:
+        raise HttpError(409, "Такая категория уже существует")
+
+    return 201, new_category
+
+
+@router.delete('/categories/{category_id}', response={204: None})
+async def delete_category(request, category_id: int):
+    try:
+        category = await Category.objects.aget(id=category_id)
+    except Category.DoesNotExist:
+        raise HttpError(404, "Категории не существует")
+    await category.adelete()
+    return 204, None
+
+
+@router.put('/categories/{category_id}', response={204: None})
+async def update_category(request, category_id: int, payload: CategoryInSchema):
+    update_data = payload.dict()
+    updated_count = await Category.objects.filter(id=category_id).aupdate(**update_data)
+    if updated_count == 0:
+        raise HttpError(404, "Категории не существует")
+    return 204, None
--- ninja_app/schemas.py
+++ ninja_app/schemas.py
@@ -1,6 +1,6 @@
 from ninja import ModelSchema
 
-from blog_app.models import Post
+from blog_app.models import Post, Category
 
 
 class PostInSchema(ModelSchema):
@@ -13,3 +13,14 @@ class PostOutSchema(ModelSchema):
     class Meta:
         model = Post
         fields = ('id', 'slug', 'title', 'content', 'category', 'author', 'published', 'created_at')
+
+
+class CategoryInSchema(ModelSchema):
+    class Meta:
+        model = Category
+        fields = ('title', 'slug')
+
+class CategoryOutSchema(ModelSchema):
+    class Meta:
+        model = Category
+        fields = ('id', 'title', 'slug')
